3aacbd76c2d913df1abc22e4379f4b83a0fc0c1b
Abdurrahim Eskin <abdurrahimeskincmpe@gmail.com>
2023-03-05 04:20:04 +0100

    Smaller Greater Element problem has been solved

---
+++ interviewQuestions/SmallestGreaterElement.py
@@ -0,0 +1,15 @@
+def greaterElement(arr, n):
+    # Complete the function
+    result = [1] * n
+    for i in range(0, n):
+        if arr[i] == max(arr):
+            result[i] = -10000000
+        else:
+            filtered = [x for x in arr if x > arr[i]]
+            filtered.sort()
+            result[i] = filtered[0]
+    return result
+
+
+arr = [13, 6, 7, 12]
+print(greaterElement(arr, 4))
