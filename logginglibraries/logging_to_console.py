4ebebf0fe009949637c2c4c6d379eb743a11557e
Abdurrahim Eskin <abdurrahimeskincmpe@gmail.com>
2023-03-12 23:13:17 +0100

    Logging examples are added.

---
+++ logginglibraries/logging_to_console.py
@@ -0,0 +1,21 @@
+import logging
+
+
+def main() -> None:
+    logging.basicConfig(
+        level=logging.WARNING,
+        format="%(asctime)s %(levelname)s %(message)s",
+        datefmt="%Y-%m-%d %H:%M:%S",
+        filename="basic.log",
+    )
+
+    logging.debug("This is a debug message.")
+    logging.info("This is a info message.")
+    logging.warning("This is a warning message.")
+    logging.error("This is an error message.")
+    logging.critical("This is a critical message.")
+
+
+if __name__ == "__main__":
+    main()
+
